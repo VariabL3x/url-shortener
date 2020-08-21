0b02abe2258392e7a1f8812a0245a7f807bddc71
kok huai <kokhuai_liew@hotmail.com>
2020-08-21 14:48:44 +0800

    public folder

--- server.js
+++ server.js
@@ -7,12 +7,24 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static('./public'))
 
-app.get('/',(req,res)=>{
-    res.json({
-        message:'Shorten your url'
-    })
+
+// app.get('/',(req,res)=>{
+//     res.json({
+//         message:'Shorten your url'
+//     })
+// })
+
+
+app.get('/:id',(req,res)=>{
+    const {id} = req.params;
 })
+
+app.post('/url',(req,res)=>{
+
+})
+
 
 app.listen(PORT,()=>{
     console.log(`Server Running on http://localhost:${PORT}`);
